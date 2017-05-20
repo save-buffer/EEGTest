0aa5e6b32b40261c9eeac379192be513625b8511
Sasha Krassovsky <warplane@hotmail.com>
2017-05-20 15:48:52 -0700

    Added FFT support

--- EEGTest/Program.cs
+++ EEGTest/Program.cs
@@ -2,161 +2,91 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Numerics;
 using System.Windows.Forms;
+using MathNet.Numerics.IntegralTransforms;
 
 namespace EEGTest
 {
     class Program
     {
-        public enum Direction
+        const string COM = "COM11";
+
+        public static void Assert(bool x)
         {
-            Up, Down, Left, Right
-        }
-        public static Direction InterpretFatSample(FatSample s)
-        {
-            if (s.Channels[15] > 0.036)
-                return Direction.Up;
-            else
-                return Direction.Left;
+            if (x)
+                return;
+            throw new Exception("Assertion was false!");
         }
 
-        public static void ShowInterpreted()
+        public static double[] FFT(double[] data)
         {
-            EEG eeg = new EEG("COM4");
-            eeg.Start();
-            int index = 0;
-            var filestream = System.IO.File.AppendText("Test1.txt");
-            while (true)
+            Complex[] samples = (from x in data select new Complex(x, 0)).ToArray();
+            Fourier.Forward(samples);
+            return (from x in samples select x.Magnitude).ToArray();
+        }
+
+        public static double[][] FFTData(double[,] data)
+        {
+            /*
+            1 2 3 4 5 6      1 1 1
+            1 2 3 4 5 6  =>  2 2 2 ...
+            1 2 3 4 5 6      3 3 3
+            */
+            double[][] result = new double[data.GetUpperBound(0) + 1][];
+            double[] x = new double[data.GetUpperBound(1) + 1];
+            for (int i = 0; i < data.GetUpperBound(0) + 1; i++)
             {
-                Application.DoEvents();
-                FatSample[] samples = new FatSample[64];
-                for (int i = 0; i < samples.Length; i++)
-                {
-                    samples[i] = new FatSample(eeg.ReadSample(), eeg.ReadSample());
-                }
-                Console.WriteLine("INDEX: " + index);
-                FatSample f = FatSample.Average(samples);
-                Direction dir = InterpretFatSample(f);
-                switch (dir)
-                {
-                    case Direction.Up:
-                        //up
-                        break;
-                    case Direction.Down:
-                        //down
-                        break;
-                    case Direction.Left:
-                        //left
-                        break;
-                    case Direction.Right:
-                        //right
-                        break;
-                    default:
-                        break;
-                }
-                Console.WriteLine(f);
-                Console.WriteLine();
+                for (int j = 0; j < x.Length; j++)
+                    x[j] = data[i, j];
+                result[i] = FFT(x);
             }
+            return result;
         }
 
-        public static void CollectData()
+        public static void CollectData(string File)
         {
             const int samples_to_collect = 1000;
-            const int samples_to_average = 1;
-            EEG eeg = new EEG("COM3");
+            EEG eeg = new EEG(COM);
             eeg.Start();
+            Console.WriteLine("Connected");
             int index = 0;
-            var filestream = System.IO.File.AppendText("Down.txt");
             List<FatSample> all_samples = new List<FatSample>();
             for (int j = 0; j < samples_to_collect; j++)
             {
-                FatSample[] samples = new FatSample[samples_to_average];
-                for (int i = 0; i < samples.Length; i++)
-                {
-                    samples[i] = new FatSample(eeg.ReadSample(), eeg.ReadSample());
-                }
+                FatSample f = new FatSample(eeg.ReadSample(), eeg.ReadSample());
                 Console.WriteLine("INDEX: " + index);
-                FatSample f = FatSample.Average(samples);
                 all_samples.Add(f);
                 Console.WriteLine(f);
                 Console.WriteLine();
             }
+            var filestream = System.IO.File.AppendText(File);
             foreach (var i in all_samples)
                 filestream.Write(i.ToSpaceSeparated());
+            eeg.Stop();
         }
 
-        static void TestNeuralNet()
+        static void CollectData2()
         {
-            const int N = 6000;
-            int[][] Layers = new int[][] { new int[] { 1, 2 }, new int[] { 1, 2, 2 }, new int[] { 1, 2, 3, 2 }, new int[] { 1, 14, 22, 2 } };
-            double[,] training_set = new double[N, 1];
-            int[] labels = new int[N];
-            Random r = new Random();
-            for (int i = 0; i < N; i++)
-            {
-                switch (i % 2)
-                {
-                    case 0:
-                        training_set[i, 0] = r.NextDouble() * 50.0;
-                        labels[i] = 0;
-                        break;
-                    case 1:
-                        training_set[i, 0] = -1 * r.NextDouble() * 50.0;
-                        labels[i] = 1;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            NeuralNet n = NeuralNet.ChooseBestNeuralNet(training_set, labels, Layers, 500);
-            Console.WriteLine(n.PercentCorrect() + "% Correct");
-            Console.WriteLine("Accepting Input");
-            for (;;)
-            {
-
-                double x = double.Parse(Console.ReadLine());
-                Console.WriteLine(n.Predict(new double[] { x }));
-            }
+            Console.WriteLine("Ready?");
+            Console.ReadLine();
+            CollectData("Up.txt");
+            Console.WriteLine("Now we're training going down");
+            Console.ReadLine();
+            CollectData("Down.txt");
+            Console.WriteLine("Done");
         }
 
         static NeuralNet NNCollectedData2()
         {
-            string up = System.IO.File.ReadAllText("Up.txt");
-            double[] ups = (from string s in up.Split(null) where s != "" select double.Parse(s)).ToArray();
-            string down = System.IO.File.ReadAllText("Down.txt");
-            double[] downs = (from string s in down.Split(null) where s != "" select double.Parse(s)).ToArray();
-
-            double[,] x = new double[ups.Length / 16 + downs.Length / 16, 16];
-            int[] y = new int[ups.Length / 16 + downs.Length / 16];
-            for (int i = 0; i < ups.Length / 16; i++)
-            {
-                for (int j = 0; j < 16; j++)
-                    x[i, j] = ups[i * 16 + j];
-                y[i] = 0;
-            }
-
-            for (int i = ups.Length; i < (ups.Length + downs.Length) / 16; i++)
-            {
-                for (int j = 0; j < 16; j++)
-                    x[i, j] = downs[(i - ups.Length) * 16 + j];
-                y[i] = 1;
-            }
-            int[][] Layers = new int[][] { new int[] { 16, 20, 2 }, new int[] { 16, 25, 15, 5, 2 }, new int[] { 16, 2 }, new int[] { 16, 3, 5, 2 } };
-            NeuralNet n = NeuralNet.ChooseBestNeuralNet(x, y, Layers, 500);
-            Console.WriteLine(n.PercentCorrect() + "% Correct");
-            string layers = "";
-            foreach (var i in n.Layers)
-                layers += i + " ";
-            Console.WriteLine("Layers: " + layers);
-            return n;
+            var raw_up = ReadDataFromFile("Up.txt");
+            var down = ReadDataFromFile("Down.txt");
+            var up = FFTData(raw_up);
         }
 
         static void EEGTestWithNN2()
         {
-            EEG eeg = new EEG("COM3");
+            EEG eeg = new EEG(COM);
             eeg.Start();
             var n = NNCollectedData2();
             Stopwatch s = new Stopwatch();
@@ -169,9 +99,67 @@ namespace EEGTest
             }
         }
 
+        public static double[,] ReadDataFromFile(string File)
+        {
+            string d = System.IO.File.ReadAllText(File);
+            double[] arr = (from s in d.Split(null) where s != "" select double.Parse(s) * 10.0e2).ToArray();
+            Assert(arr.Length % 16 == 0);
+            double[,] data = new double[16, d.Length / 16];
+            for (int i = 0; i < arr.Length; i++)
+                data[i % 16, i / 16] = arr[i];
+            return data;
+        }
+
+        const double Delta = 3.0;
+        const double Theta = 7.0;
+        const double Alpha = 12.0;
+        const double SMR_Beta = 15.0;
+        const double MID_Beta = 18.0;
+        const double HI_Beta = 35.0;
+        //Gamma?
+
+        //Hz = Frequency, Fs = Sampling Rate, N = Size of FFT, n = index
+        // Hz = n * Fs / N => n = N * Hz / Fs
+
+        static int FFTIndex(double Hz, int SamplingRate, int N) => (int)(N * Hz / SamplingRate);
+
+        static FFT_Sample[] SelectFFT_Sample(double[,] RawData)
+        {
+            var fft = FFTData(RawData);
+            FFT_Sample[] result = new FFT_Sample[fft.Length];
+            for (int i = 0; i < fft.Length; i++)
+            {
+                result[i].Delta = fft[i][FFTIndex(Delta, 250, fft[i].Length)];
+                result[i].Theta = fft[i][FFTIndex(Theta, 250, fft[i].Length)];
+                result[i].Alpha = fft[i][FFTIndex(Alpha, 250, fft[i].Length)];
+                result[i].SMR_Beta = fft[i][FFTIndex(SMR_Beta, 250, fft[i].Length)];
+                result[i].MID_Beta = fft[i][FFTIndex(MID_Beta, 250, fft[i].Length)];
+                result[i].HI_Beta = fft[i][FFTIndex(HI_Beta, 250, fft[i].Length)];
+            }
+            return result;
+        }
+
+        static void TestFFT()
+        {
+            var data = ReadDataFromFile("Up.txt");
+            var fft = FFTData(data);
+            for (int i = 0; i < fft.Length; i++)
+            {
+                Console.WriteLine($"Channel {i}:");
+                Console.WriteLine($"\tDelta   : {fft[i][FFTIndex(Delta, 250, fft[i].Length)]}");
+                Console.WriteLine($"\tTheta   : {fft[i][FFTIndex(Theta, 250, fft[i].Length)]}");
+                Console.WriteLine($"\tAlpha   : {fft[i][FFTIndex(Alpha, 250, fft[i].Length)]}");
+                Console.WriteLine($"\tSMR_Beta: {fft[i][FFTIndex(SMR_Beta, 250, fft[i].Length)]}");
+                Console.WriteLine($"\tMID_Beta: {fft[i][FFTIndex(MID_Beta, 250, fft[i].Length)]}");
+                Console.WriteLine($"\tHI_Beta : {fft[i][FFTIndex(HI_Beta, 250, fft[i].Length)]}");
+                Console.WriteLine();
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            EEGTestWithNN2();
+            CollectData2();
             Console.ReadLine();
         }
     }
